1f4c9e97588af61c286acd1fcef94aa96802fbf7
janicek <janicek@spsejecna.cz>
2024-03-18 09:36:45 +0100

    added TicTac toe class and its methods

---
+++ src/TicTacToe.java
@@ -0,0 +1,29 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe implements ActionListener {
+    public TicTacToe() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a,int b, int c) {
+
+    }
+    public void oWins(int a,int b, int c) {
+
+    }
+
+}
